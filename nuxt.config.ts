3c07ec62aa027708f4b196e9e2b61fd30a1f4389
anntptwendeesoft <adamaanvaan@gmail.com>
2023-07-03 18:10:10 +0700

    Fix url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,7 @@ export default defineNuxtConfig({
       STRAVA_CLIENT_ID: process.env.STRAVA_CLIENT_ID,
       STRAVA_BASE_URL: process.env.STRAVA_BASE_URL,
       STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET,
+      BASE_URL: process.env.BASE_URL,
     },
   },
 });
