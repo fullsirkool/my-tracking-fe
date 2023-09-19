dacca19b54803e8b5ff5f09be4dbc261caf681f9
andrew <anntp@twendeesoft.com>
2023-09-19 14:37:30 +0700

    change api methods

--- pages/exchange-token.vue
+++ pages/exchange-token.vue
@@ -4,35 +4,28 @@
 <script setup>
 const { query } = useRoute();
 const runtimeConfig = useRuntimeConfig();
-const { STRAVA_CLIENT_ID, STRAVA_BASE_URL, STRAVA_CLIENT_SECRET, BASE_URL } = runtimeConfig.public;
+const {
+  STRAVA_CLIENT_ID,
+  STRAVA_BASE_URL,
+  STRAVA_CLIENT_SECRET,
+  BASE_URL,
+} = runtimeConfig.public;
 const { code } = query;
 
 const exchangeToken = async () => {
-  // const url = `${STRAVA_BASE_URL}/oauth/token`;
-  // console.log("code", code);
-  // const { data } = await useFetch(url, {
-  //   method: "post",
-  //   query: {
-  //     client_id: STRAVA_CLIENT_ID,
-  //     client_secret: STRAVA_CLIENT_SECRET,
-  //     code: code,
-  //     grant_type: `authorization_code`,
-  //   },
-  // });
-  // console.log("exchangeToken data", data._rawValue);
-  // return data._rawValue;
-  const url = `${BASE_URL}/auth/signin/${code}`;
-  const res = await useFetch(url, {
+  const url = `${STRAVA_BASE_URL}/oauth/token`;
+  console.log("code", code);
+  const { data } = await useFetch(url, {
     method: "post",
-    // query: {
-    //   client_id: STRAVA_CLIENT_ID,
-    //   client_secret: STRAVA_CLIENT_SECRET,
-    //   code: code,
-    //   grant_type: `authorization_code`,
-    // },
+    query: {
+      client_id: STRAVA_CLIENT_ID,
+      client_secret: STRAVA_CLIENT_SECRET,
+      code: code,
+      grant_type: `authorization_code`,
+    },
   });
-  console.log("exchangeToken data", res);
-  return res;
+  console.log("exchangeToken data", data._rawValue);
+  return data._rawValue;
 };
 
 exchangeToken();
