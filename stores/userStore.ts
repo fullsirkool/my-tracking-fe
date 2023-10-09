9d932e1c89ea462e9e092900303595bcc4befb04
andrew <adamaanvaan@gmail.com>
2023-10-09 16:33:29 +0700

    Fix change log

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,7 +1,11 @@
 import { defineStore } from "pinia";
 
 export const useUserStore = defineStore("user", () => {
-  const user = ref(JSON.parse(localStorage.getItem('user-info')) || {});
+  const user = ref(
+    localStorage.getItem("user-info")
+      ? JSON.parse(localStorage.getItem("user-info"))
+      : {}
+  );
 
   const setUser = (u) => {
     user.value = u;
