e026034f96aef9e994a5f119c10086dffd1b20cc
andrew <adamaanvaan@gmail.com>
2023-10-18 14:23:02 +0700

    Log cookie

--- stores/userStore.ts
+++ stores/userStore.ts
@@ -1,4 +1,6 @@
 import { defineStore } from "pinia";
+const runtimeConfig = useRuntimeConfig();
+const { BASE_URL } = runtimeConfig.public;
 
 export const useUserStore = defineStore("user", () => {
   const user = ref({});
@@ -8,13 +10,33 @@ export const useUserStore = defineStore("user", () => {
   };
 
   const initValue = () => {
+    const accessTokenCookie = useCookie("access-token");
+    const refreshTokenCookie = useCookie("refresh-token");
+    console.log('accessTokenCookie', accessTokenCookie)
+    console.log('refreshTokenCookie', refreshTokenCookie)
+    // if (!accessTokenCookie && !refreshTokenCookie) {
+    //   return;
+    // }
+
+    // if (!accessTokenCookie) {
+    // }
+
     const loadedInfo = localStorage.getItem("user-info");
     if (typeof loadedInfo === "string") {
       user.value = JSON.parse(loadedInfo);
     }
   };
 
-  initValue()
+  const recallToken = async () => {
+    const { data } = await useFetch(`${BASE_URL}/auth/renew`, {
+      method: "POST",
+      body: {
+
+      }
+    });
+  };
+
+  initValue();
 
   return {
     user,
