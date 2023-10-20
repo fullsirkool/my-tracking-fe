80d54774a86404b776cdb9be98d32045799b8575
andrew <adamaanvaan@gmail.com>
2023-10-20 17:53:33 +0700

    Fix middleware routing

--- middleware/authentication.ts
+++ middleware/authentication.ts
@@ -3,15 +3,14 @@ import { useUserStore } from "~/stores/userStore";
 
 export default defineNuxtRouteMiddleware((to, from) => {
   console.log("from", from);
-  const userStore = useUserStore();
-  const { user } = storeToRefs(userStore);
-  console.log("middleware", user._rawValue, isEmpty(user._rawValue));
-  
-  if (isEmpty(user._rawValue)) {
+  const { user } = useUserStore();
+  console.log("middleware", user, isEmpty(user));
+
+  if (isEmpty(user)) {
     return navigateTo("/login");
   }
 
   if (from.fullPath === "/profile") {
-    return navigateTo(`profile/${user.value.stravaId}`);
+    return navigateTo(`profile/${user?.stravaId}`);
   }
 });
