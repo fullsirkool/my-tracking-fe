0cf9a384a8092eb203c7f6cb59758b488911c77d
andrew <adamaanvaan@gmail.com>
2023-11-03 15:38:02 +0700

    Fix Responsive

--- components/Challenge/ProcessTable.vue
+++ components/Challenge/ProcessTable.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-10">
-    <div class="grid grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       <ChallengeProcessCard v-for="item in challengeUsers" :user-activitites="item" :challenge="challengeDetail"
         class="grid-span-1">
       </ChallengeProcessCard>
