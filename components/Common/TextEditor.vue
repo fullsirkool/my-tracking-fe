d8950c67d45b56ee88a3f5a09793072374c16d54
minh duc nguyen <minhducnguyenpsy@gmail.com>
2024-07-05 17:44:34 +0700

    feat: apply rich text editor to description challenge

--- components/Common/TextEditor.vue
+++ components/Common/TextEditor.vue
@@ -1,136 +1,171 @@
 <template>
-  <div>
-    <div v-if="editor">
-      <button
-          @click="editor.chain().focus().toggleBold().run()"
-          :disabled="!editor.can().chain().focus().toggleBold().run()"
-          :class="{ 'is-active': editor.isActive('bold') }"
+  <div class="shadow-sm">
+    <div
+      v-if="editor"
+      class="te-bar flex gap-1 border border-b-0 border-slate-100 rounded-tl-xl rounded-tr-xl p-1"
+    >
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleBold().run()"
+        :disabled="!editor.can().chain().focus().toggleBold().run()"
+        :class="{ 'is-active': editor.isActive('bold') }"
       >
-        bold
-      </button>
-      <button
-          @click="editor.chain().focus().toggleItalic().run()"
-          :disabled="!editor.can().chain().focus().toggleItalic().run()"
-          :class="{ 'is-active': editor.isActive('italic') }"
+        <Icon name="mdi:format-bold" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleItalic().run()"
+        :disabled="!editor.can().chain().focus().toggleItalic().run()"
+        :class="{ 'is-active': editor.isActive('italic') }"
       >
-        italic
-      </button>
-      <button
-          @click="editor.chain().focus().toggleStrike().run()"
-          :disabled="!editor.can().chain().focus().toggleStrike().run()"
-          :class="{ 'is-active': editor.isActive('strike') }"
+        <Icon name="mdi:format-italic" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleStrike().run()"
+        :disabled="!editor.can().chain().focus().toggleStrike().run()"
+        :class="{ 'is-active': editor.isActive('strike') }"
       >
-        strike
-      </button>
-      <button
-          @click="editor.chain().focus().toggleCode().run()"
-          :disabled="!editor.can().chain().focus().toggleCode().run()"
-          :class="{ 'is-active': editor.isActive('code') }"
+        <Icon name="ic:baseline-strikethrough-s" width="20" height="20" />
+      </UButton>
+      <!-- <UButton variant="ghost"
+        @click="editor.chain().focus().toggleCode().run()"
+        :disabled="!editor.can().chain().focus().toggleCode().run()"
+        :class="{ 'is-active': editor.isActive('code') }"
       >
         code
-      </button>
-      <button @click="editor.chain().focus().unsetAllMarks().run()">
+      </UButton> -->
+      <!-- <UButton variant="ghost" @click="editor.chain().focus().unsetAllMarks().run()">
         clear marks
-      </button>
-      <button @click="editor.chain().focus().clearNodes().run()">
+      </UButton> -->
+      <!-- <UButton variant="ghost" @click="editor.chain().focus().clearNodes().run()">
         clear nodes
-      </button>
-      <button
-          @click="editor.chain().focus().setParagraph().run()"
-          :class="{ 'is-active': editor.isActive('paragraph') }"
+      </UButton> -->
+      <UButton variant="ghost"
+        @click="editor.chain().focus().setParagraph().run()"
+        :class="{ 'is-active': editor.isActive('paragraph') }"
       >
-        paragraph
-      </button>
-      <button
-          @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-          :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+        <Icon name="mdi:format-paragraph" width="20" height="20"/>
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
       >
-        h1
-      </button>
-      <button
-          @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-          :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+        <Icon name="gravity-ui:heading-1" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
       >
-        h2
-      </button>
-      <button
-          @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-          :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+        <Icon name="gravity-ui:heading-2" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+        :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
       >
-        h3
-      </button>
-      <button
-          @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-          :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+        <Icon name="gravity-ui:heading-3" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+        :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
       >
-        h4
-      </button>
-      <button
-          @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
-          :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+        <Icon name="gravity-ui:heading-4" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+        :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
       >
-        h5
-      </button>
-      <button
-          @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
-          :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+        <Icon name="gravity-ui:heading-5" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+        :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
       >
-        h6
-      </button>
-      <button
-          @click="editor.chain().focus().toggleBulletList().run()"
-          :class="{ 'is-active': editor.isActive('bulletList') }"
+        <Icon name="gravity-ui:heading-6" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleBulletList().run()"
+        :class="{ 'is-active': editor.isActive('bulletList') }"
       >
-        bullet list
-      </button>
-      <button
-          @click="editor.chain().focus().toggleOrderedList().run()"
-          :class="{ 'is-active': editor.isActive('orderedList') }"
+        <Icon name="mdi:format-list-bulleted" width="20" height="20" />
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().toggleOrderedList().run()"
+        :class="{ 'is-active': editor.isActive('orderedList') }"
       >
-        ordered list
-      </button>
-      <button
-          @click="editor.chain().focus().toggleCodeBlock().run()"
-          :class="{ 'is-active': editor.isActive('codeBlock') }"
+        <Icon name="mdi:format-list-numbers" width="20" height="20" />
+      </UButton>
+      <!-- <UButton variant="ghost"
+        @click="editor.chain().focus().toggleCodeBlock().run()"
+        :class="{ 'is-active': editor.isActive('codeBlock') }"
       >
         code block
-      </button>
-      <button
-          @click="editor.chain().focus().toggleBlockquote().run()"
-          :class="{ 'is-active': editor.isActive('blockquote') }"
+      </UButton> -->
+      <!-- <UButton variant="ghost"
+        @click="editor.chain().focus().toggleBlockquote().run()"
+        :class="{ 'is-active': editor.isActive('blockquote') }"
       >
         blockquote
-      </button>
-      <button @click="editor.chain().focus().setHorizontalRule().run()">
+      </UButton> -->
+      <!-- <UButton variant="ghost" @click="editor.chain().focus().setHorizontalRule().run()">
         horizontal rule
-      </button>
-      <button @click="editor.chain().focus().setHardBreak().run()">
+      </UButton> -->
+      <!-- <UButton variant="ghost" @click="editor.chain().focus().setHardBreak().run()">
         hard break
-      </button>
-      <button
-          @click="editor.chain().focus().undo().run()"
-          :disabled="!editor.can().chain().focus().undo().run()"
+      </UButton> -->
+      <!-- <UButton variant="ghost"
+        @click="editor.chain().focus().undo().run()"
+        :disabled="!editor.can().chain().focus().undo().run()"
       >
         undo
-      </button>
-      <button
-          @click="editor.chain().focus().redo().run()"
-          :disabled="!editor.can().chain().focus().redo().run()"
+      </UButton>
+      <UButton variant="ghost"
+        @click="editor.chain().focus().redo().run()"
+        :disabled="!editor.can().chain().focus().redo().run()"
       >
         redo
-      </button>
+      </UButton> -->
     </div>
-    <TiptapEditorContent :editor="editor" />
+    <TiptapEditorContent :editor="editor" class="p-2.5" />
   </div>
 </template>
 
 <script setup>
+// const modelValue = defineModel()
+const props = defineProps({
+  modelValue: String,
+})
+
+const emit = defineEmits(['update:model-value'])
 const editor = useEditor({
-  content: "<p>I'm running Tiptap with Vue.js. 🎉</p>",
   extensions: [TiptapStarterKit],
-});
+  onBlur: (event) => {
+    console.log('event.editor.getText()', event.editor.getText())
+    console.log('event.editor.getHTML()', event.editor.getHTML())
+    console.log('event.editor.getJSON()', event.editor.getJSON())
+
+    // emit('update:model-value', event.editor.getText())
+    emit('update:model-value', event.editor.getHTML())
+  },
+})
+
+onMounted(() => {
+  if (!!unref(editor)) {
+    unref(editor).commands.setContent(props.modelValue)
+    // console.log(props.modelValue)
+  }
+})
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    unref(editor).commands.setContent(newValue)
+    unref(editor).getHTML()
+  },
+)
 
 onBeforeUnmount(() => {
-  unref(editor).destroy();
-});
+  unref(editor).destroy()
+})
 </script>
+
+<style scoped lang="scss">
+/* te: text-editor */
+</style>
