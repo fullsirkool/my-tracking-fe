1d99cf68186dfc35d82be69c670717b4923652bf
Andrew Ngo <adamaanvaan@gmail.com>
2024-07-06 11:01:27 +0700

    Fix home UI

--- components/Challenge/ImageSlider.vue
+++ components/Challenge/ImageSlider.vue
@@ -2,41 +2,41 @@
   <carousel wrap-around :autoplay="40000">
     <slide v-for="challenge in challenges" :key="challenge.id" class="relative">
       <div
-        class="slide-cover w-full object-cover rounded-xl h-1/2"
-        :style="{ background: `url(${challenge.image})` }"
+          class="slide-cover w-full object-cover rounded-xl h-1/2"
+          :style="{ background: `url(${challenge.image})` }"
       />
       <div class="absolute bottom-0 left-0 p-10 w-full md:flex justify-between">
         <div class="text-left w-full md:w-1/2">
           <h1
-            class="text-3xl md:text-4xl font-semibold text-left text-slate-100 tracking-wide leading-10 drop-shadow mb-2.5"
+              class="text-3xl md:text-4xl font-semibold text-left text-slate-100 tracking-wide leading-10 drop-shadow mb-2.5"
           >
             <span>{{ challenge.title }}</span>
           </h1>
           <div
-            class="text-white text-sm mb-5 md:text-lg leading-4 md:leading-6 font-light md:min-h-[80px] line-clamp-3 text-ellipsis overflow-hidden ..."
+              class="text-white text-sm mb-5 md:text-lg leading-4 md:leading-6 font-light md:min-h-[80px] line-clamp-3 text-ellipsis overflow-hidden ..."
           >
-            {{ challenge.description }}
+            <div v-html="challenge.description"/>
           </div>
         </div>
         <div class="flex items-end md:justify-between">
           <NuxtLink
-            :to="`/challenge/${challenge.id}`"
-            class="mt-1 text-md flex items-center gap-2 font-bold hover:text-primary-600 hover:bg-white hover:translate-x-1 transition-transform bg-primary-600 text-white px-5 py-2.5 rounded-xl shadow-2xl"
+              :to="`/challenge/${challenge.id}`"
+              class="mt-1 text-md flex items-center gap-2 font-bold hover:text-primary-600 hover:bg-white hover:translate-x-1 transition-transform bg-primary-600 text-white px-5 py-2.5 rounded-xl shadow-2xl"
           >
             {{ $t('join') }}
-            <UIcon name="i-heroicons-arrow-right-20-solid" />
+            <UIcon name="i-heroicons-arrow-right-20-solid"/>
           </NuxtLink>
         </div>
       </div>
     </slide>
     <template #addons>
-      <navigation />
-      <pagination />
+      <navigation/>
+      <pagination/>
     </template>
   </carousel>
 </template>
 <script setup lang="ts">
-import { Challenge } from '~/types/dto/challenge.dto'
+import {Challenge} from '~/types/dto/challenge.dto'
 
 interface IChallengeImageSlider {
   challenges: Challenge[]
